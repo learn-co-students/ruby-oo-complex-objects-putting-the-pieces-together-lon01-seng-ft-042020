1df129f56744dbd09ad0233225ab24a10ae3f76c
github username <github email address>
2020-04-23 20:44:17 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+#Define a Book class
+class Book
 
+  #Define the attributes of a Book instance
+  attr_accessor :author, :page_count, :genre
+
+  #Define a macro reader method for title
+  attr_reader :title
+
+  #initialize with a title
+  def initialize(title)
+   @title = title 
+  end 
+  
+  #Define the behaviour of a Book instance
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
