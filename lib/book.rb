86766a7178e7a7fda9415364f8a98cd62d5ce0a1
ewelinaszoda <ewelinaszoda@gmail.com>
2020-04-25 11:02:43 +0200

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,60 @@
+class Book
 
+    attr_accessor :author, :page_count, :genre
+     
+    attr_reader :title 
+
+    def initialize (title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+    
+end
+
+class Book
+
+    def initialize (title)
+        @title = title
+    end 
+
+    # instance method reader
+    def title
+        @title
+    end 
+
+
+    # giving properties, setter, writer
+    def author=(author)
+        @author = author
+    end 
+
+    # reader
+    def author
+        @author
+    end 
+
+    def page_count=(num)
+        @page_count = num
+    end 
+
+    def page_count
+        @page_count
+    end 
+
+    def genre=(genre)
+        @genre = genre
+    end 
+
+    def genre
+        @genre
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+    
+end
 
